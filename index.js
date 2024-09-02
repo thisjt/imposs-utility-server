600c8cac52a8d8088baa1648393da7992c8b4b53
Joshua Tahanlangit <contact@thisjt.me>
2024-09-03 03:39:04 +0800

    refactor: actually use the endpoints in index

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import 'dotenv/config';
 import backup from './endpoints/backup/index.js';
+import analytics from './endpoints/analytics/index.js';
+import logging from './endpoints/logging/index.js';
+import status from './endpoints/status/index.js';
 
 const PORT = 3030;
 const app = express();
@@ -9,6 +12,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 backup(app);
+analytics(app);
+logging(app);
+status(app);
 
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
