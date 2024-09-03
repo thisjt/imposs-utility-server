d0afdbca2b77b9cd9680f5de6b65b467d2a4ace9
Joshua Tahanlangit <contact@thisjt.me>
2024-09-03 12:11:49 +0800

    feat: log status and logging to file

--- endpoints/status/index.js
+++ endpoints/status/index.js
@@ -1,7 +1,26 @@
+import fs from 'fs';
+
+if (!fs.existsSync('./logs')) fs.mkdirSync('./logs');
+
+let loggerFilename = `status-${new Date().toLocaleDateString().replaceAll('/', '-')}.log`;
+setInterval(() => {
+	loggerFilename = `status-${new Date().toLocaleDateString().replaceAll('/', '-')}.log`;
+}, 1000 * 60 * 30);
+
+function writeToFile(data, retries = 0) {
+	fs.appendFile(`./logs/${loggerFilename}`, `${data}`, (err) => {
+		if (err && retries < 10) {
+			retries++;
+			setTimeout(() => writeToFile(data, retries), 100 + Math.floor(Math.random() * 1000));
+		}
+	});
+}
+
 /** @param {import('express').Express} app */
 export default function status(app) {
 	app.post('/status', (req, res) => {
 		console.log('status accessed');
+		writeToFile(JSON.stringify(req.body));
 		res.status(200).json({});
 	});
 }
